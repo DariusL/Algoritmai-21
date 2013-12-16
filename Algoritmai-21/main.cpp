aef97b5649a35bea98aba512972e39ba2d7151e0
DariusL <d.lapunas@gmail.com>
2013-12-16 20:01:27 +0200

    done
    What the fuck am i doing edition

--- Algoritmai-21/main.cpp
+++ Algoritmai-21/main.cpp
@@ -2,13 +2,14 @@
 #include <memory>
 #include <random>
 #include <functional>
+#include <numeric>
 
 using namespace std;
 
 struct Element
 {
-	size_t key, p, q;
-	Element(size_t key, size_t p, size_t q) :key(key), p(p), q(q){}
+	size_t key, p;
+	Element(size_t key, size_t p) :key(key), p(p){}
 };
 
 struct Node
@@ -21,97 +22,71 @@ struct Node
 void ComputeCRW(const vector<Element> &data, vector < vector < size_t >> &c, vector < vector < size_t >> &r, vector < vector < size_t >> &w);
 unique_ptr<Node> ConstructTree(const vector < vector < size_t >> &c, const vector < vector < size_t >> &r, const vector < vector < size_t >> &w, size_t i, size_t j);
 
-int clamp(int n, int min);
-
 int main()
 {
-	const size_t n = 6;
+	const size_t n = 10;
 	vector < vector < size_t >> c, r, w;
 	vector<Element> data;
 	default_random_engine gen;
 	uniform_int_distribution<size_t> dist(0, 10);
 	auto num = bind(dist, gen);
-	for (size_t i = 0; i < n; i++)
+	for (size_t i = 1; i <= n+2; i++)
 	{
 		c.emplace_back();
 		r.emplace_back();
 		w.emplace_back();
-		for (size_t j = 0; j < n; j++)
+		for (size_t j = 1; j <= n+1; j++)
 		{
-			c[i].emplace_back();
-			r[i].emplace_back();
-			w[i].emplace_back();
+			c[i - 1].emplace_back();
+			r[i - 1].emplace_back();
+			w[i - 1].emplace_back();
 		}
+	}
+	for (size_t i = 1; i <= n; i++)
+	{
 		size_t p = num();
-		size_t q = num();
-		data.emplace_back(i, p, q);
+		data.emplace_back(i, p);
 	}
 	ComputeCRW(data, c, r, w);
-	auto tree = ConstructTree(c, r, w, 0, n - 1);
+	auto tree = ConstructTree(c, r, w, 1, n);
 	return 0;
 }
 
 void ComputeCRW(const vector<Element> &data, vector < vector < size_t >> &c, vector < vector < size_t >> &r, vector < vector < size_t >> &w)
 {
-	size_t n = data.size(), j, m, x, min;
+	size_t j, max = numeric_limits<size_t>::max(), t, n = data.size();
 
-	//Construct weight matrix W 
-	for (size_t i = 0; i < n; i++)
+	for (size_t i = 1; i <= n+1; i++)
 	{
-		w[i][i] = data[i].q;
-		for (size_t j = i + 1; j < n; j++)
+		c[i][i - 1] = 0;
+		w[i][i - 1] = 0;
+	}
+	for (size_t l = 1; l <= n; l++)
+	{
+		for (size_t i = 1; i <= n - l + 1; i++)
 		{
-			w[i][j] = w[i][j - 1] + data[j].p + data[j].q;
-		}
-	}
-
-	//Construct cost matrix C and root matrix R 
-	for (size_t i = 0; i < n; i++)
-	{
-		c[i][i] = w[i][i];
-	}
-	for (size_t i = 0; i < n - 1; i++)
-	{
-		j = i + 1;
-		c[i][j] = c[i][i] + c[j][j] + w[i][j];
-		r[i][j] = j;
-	}
-	for (size_t h = 2; h < n; h++)
-	{
-		for (size_t i = 0; i < n - h; i++)
-		{
-			j = i + h;
-			m = r[i][j - 1];
-			min = c[i][m - 1] + c[m][j];
-			for (size_t k = m + 1; k < r[i + 1][j]; k++)
+			j = i + l - 1;
+			c[i][j] = max;
+			w[i][j] = w[i][j - 1] + data[j-1].p;
+			for (size_t m = i; m <= j; m++)
 			{
-				x = c[i][k - 1] + c[k][j];
-				if (x < min)
+				t = c[i][m - 1] + c[m + 1][j] + w[i][j];
+				if (t < c[i][j])
 				{
-					m = k;
-					min = x;
+					c[i][j] = t;
+					r[i][j] = m;
 				}
 			}
-			c[i][j] = w[i][j] + min;
-			r[i][j] = m;
 		}
 	}
 }
 
 unique_ptr<Node> ConstructTree(const vector < vector < size_t >> &c, const vector < vector < size_t >> &r, const vector < vector < size_t >> &w, size_t i, size_t j)
 {
-	if (i != j)
+	if (i <= j)
 	{
 		size_t root = r[i][j];
-		return make_unique<Node>(root, ConstructTree(c, r, w, i, root - 1), ConstructTree(c, r, w, root, j));
+		return make_unique<Node>(root, ConstructTree(c, r, w, i, root - 1), ConstructTree(c, r, w, root + 1, j));
 	}
 	return nullptr;
-}
-
-int clamp(int n, int min)
-{
-	if (n > min)
-		return n;
-	else
-		return min;
 }
